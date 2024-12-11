95f66b8e13ac5f5adeba7268fea7ec86ef17d207
Sandi Miftah <sandiseeyou@gmail.com>
2024-12-11 15:59:49 +0700

    fix rank list section

--- src/components/RankPoster.jsx
+++ src/components/RankPoster.jsx
@@ -1,8 +1,14 @@
 export default function RankPoster({ description, image, rating, title }) {
   return (
     <>
-      <div className="flex flex-row v-[20vh]">
-        <div className="w-20 h-40 bg-pink"></div>
+      <div className="flex flex-row w-full border-8 border-[pink] gap-4">
+        <div className="flex flex-col items-center justify-center text-4xl text-white font-bold bg-purple-500 w-64 h-auto rounded shadow-lg">
+          <p>Movie 1</p>
+        </div>
+        <div className="flex flex-col p-8 justify-start text-4xl text-white font-bold bg-yellow-500 w-full h-auto rounded shadow-lg gap-4">
+          <p>Title</p>
+          <p>Description</p>
+        </div>
       </div>
     </>
   );
--- src/routes/RankList.jsx
+++ src/routes/RankList.jsx
@@ -4,7 +4,16 @@ export default function RankList() {
   return (
     <>
       <Outlet />
-      <section id="rank-list">
+      <section id="rank-list" className="flex flex-col h-full py-8 px-8">
+        <RankPoster />
+        <RankPoster />
+        <RankPoster />
+        <RankPoster />
+        <RankPoster />
+        <RankPoster />
+        <RankPoster />
+        <RankPoster />
+        <RankPoster />
         <RankPoster />
       </section>
     </>
