9a7581431c08a06492e273e12ec166b45921e3b5
Sandi Miftah <sandiseeyou@gmail.com>
2025-01-08 11:00:22 +0700

    finishing simple dummy popup sign in

--- src/components/NavMenu.jsx
+++ src/components/NavMenu.jsx
@@ -1,7 +1,22 @@
+import React from "react";
 import { Link } from "react-router-dom";
-import { Button } from "@material-tailwind/react";
+import { useState } from "react";
+import {
+  Button,
+  Dialog,
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Typography,
+  Input,
+  Checkbox,
+} from "@material-tailwind/react";
 
 export default function NavMenu() {
+  const [open, setOpen] = useState(false);
+  const openHandle = () => setOpen((cur) => !cur);
+
   return (
     <>
       <nav className="flex flex-row items-center text-md text-primaryColor space-x-16 font-bold">
@@ -9,10 +24,57 @@ export default function NavMenu() {
         <Link to="/review">Review</Link>
         <Link to="/about">About</Link>
         <Link to="/contact">Contact</Link>
-        <Button className="text-white bg-primaryColor px-12 py-4 shadow-sm">
-          <Link to={"/login"}>Login</Link>
+        <Button
+          onClick={openHandle}
+          className="text-white bg-primaryColor px-12 py-4 shadow-sm"
+        >
+          Login
         </Button>
       </nav>
+      <Dialog
+        size="xs"
+        open={open}
+        handler={openHandle}
+        className="bg-transparent shadow-none"
+      >
+        <Card className="mx-auto w-full max-w-[24rem] pb-4">
+          <CardBody className="flex flex-col gap-4">
+            <Typography className="text-center" variant="h4" color="black">
+              Login
+            </Typography>
+            <Typography
+              className="mb-3 font-normal text-center"
+              variant="paragraph"
+              color="gray"
+            >
+              Enter your username and password to login.
+            </Typography>
+            <Typography className="-mb-2" variant="h6">
+              Username
+            </Typography>
+            <Input label="Username" size="lg" />
+            <Typography className="-mb-2" variant="h6">
+              Password
+            </Typography>
+            <Input label="Password" size="lg" />
+            <div className="-ml-2.5 -mt-3">
+              <Checkbox label="Remember Me" />
+            </div>
+          </CardBody>
+          <CardFooter className="pt-0">
+            <Link to="/post">
+              <Button
+                to="/post"
+                className="bg-primaryColor"
+                onClick={openHandle}
+                fullWidth
+              >
+                Login
+              </Button>
+            </Link>
+          </CardFooter>
+        </Card>
+      </Dialog>
     </>
   );
 }
